56e9a7375618cecff000bc85003985f64f98c76f
W Dean Markley <wdeanm@gmail.com>
2016-05-23 16:41:58 +0000

    g

---
+++ Week05-SolarExplorer/public/javascripts/main.js
@@ -0,0 +1,35 @@
+var elfApp = angular.module("elfApp");
+	
+elfApp.controller('MainController', function($scope, $http) {
+    'use strict';
+    $scope.mainData = "Main Data";
+    $scope.index = 0;
+    
+    $scope.getRenewable = function() {
+        console.log('getRenewable');
+        $http.get('data/Renewable.json')
+            .then(function(res) {
+                console.log(res.data[0]);
+                $scope.renewable = res.data;
+    //          $scope.renewableUtils = renewablesUtils;    
+    //          $scope.simpleFormat = renewables ;  
+            });
+    }
+});
+	
+elfApp.directive('elfRenewable', function() {
+    'use strict';
+    return {
+        controller: 'MainController',
+        templateUrl: 'renewable'
+//        controllerAs: 'mainController',
+//        template: 'First: {{mainController.renewable[mainController.index].Year}} ' +
+//        '<br>Solar: {{mainController.renewable[mainController.index]["Solar (quadrillion Btu)"]}}' +
+//        '<br>Geothermal: {{mainController.renewable[mainController.index]["Geothermal (quadrillion Btu)"]}}' +
+//        '<br>Other biomass: {{mainController.renewable[mainController.index]["Other biomass (quadrillion Btu)"]}}' +
+//        '<br>Wind power: {{mainController.renewable[mainController.index]["Wind power (quadrillion Btu)"]}}' +
+//        '<br>Liquid biofuels: {{mainController.renewable[mainController.index]["Liquid biofuels (quadrillion Btu)"]}}' +
+//         '<br>Wood biomass: {{mainController.renewable[mainController.index]["Wood biomass (quadrillion Btu)"]}}' +
+//        '<br>Hydropower: {{mainController.renewable[mainController.index]["Hydropower (quadrillion Btu)"]}}' 
+    };
+});
