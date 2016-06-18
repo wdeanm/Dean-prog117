aff28ec239dff1fe99acc5e0b07983ac2b7c4061
W Dean Markley <wdeanm@gmail.com>
2016-06-18 04:07:13 +0000

    a

--- Week04-AngularDirectiveTesting/routes/index.js
+++ Week04-AngularDirectiveTesting/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 'use strict';
-  res.render('index', { title: 'Week04-AngularDirectiveTesting' });
+router.get('/', function(req, res, next) {
+    'use strict';
+    res.render('index', {
+        title: 'Week04-AngularDirectiveTesting'
+    });
 });
 
 router.get('/:id', function(req, res, nest) {
